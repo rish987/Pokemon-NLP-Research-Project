79869f67d9719206a9d173bc4b83ba5c2cf76bf7
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2018-02-06 07:24:16 -0800

    Implement word extractor with unwieldy regex.

--- code/data_vector_constructor.py
+++ code/data_vector_constructor.py
@@ -1,10 +1,36 @@
 # Created: 31/01/2018
 import enchant;
+import re;
 
 from constants import *;
 
 # English dictionary
 j = enchant.Dict('en_US');
+
+# matches any punctuation
+DESCRIPTOR_FORMAT_ANY_PUNC = '(?:[\n.,\/#!$&\*;:{}=\-_`~()\'\"])?'
+DESCRIPTOR_FORMAT_SPACES = '\s+';
+DESCRIPTOR_FORMAT_WORD = '\w+';
+
+# matches any word before a match
+DESCRIPTOR_FORMAT_BEFORE = DESCRIPTOR_FORMAT_ANY_PUNC + \
+    DESCRIPTOR_FORMAT_WORD + DESCRIPTOR_FORMAT_ANY_PUNC + \
+    DESCRIPTOR_FORMAT_SPACES;
+
+# matches any word after a match
+DESCRIPTOR_FORMAT_AFTER = DESCRIPTOR_FORMAT_ANY_PUNC + \
+    DESCRIPTOR_FORMAT_SPACES + \
+    DESCRIPTOR_FORMAT_ANY_PUNC + DESCRIPTOR_FORMAT_WORD + \
+    DESCRIPTOR_FORMAT_ANY_PUNC;
+
+
+# number of surrounding words to match
+NUM_SURR_WORDS = 2;
+
+# string to match descriptor, in its plural forms as well (ending in 'es' or
+# 's'), both separate and unseparate from other words
+DESCRIPTOR_FORMAT_SEPARATE = r'(\b%s(?:e?s)?\b)';
+DESCRIPTOR_FORMAT_UNSEPARATE = r'(%s(?:e?s)?)';
 
 class Descriptor():
     def __init__(self, _title):
@@ -26,8 +52,30 @@ class Descriptor():
                 self.word_map[word] += 1;
 
 
-descriptors = [];
+descriptor_strings = [];
 with open(DESCRIPTORS_FILE) as f:
-    descriptors = f.read().splitlines();
+    descriptor_strings = f.read().splitlines();
 
-print(descriptors);
+# to hold all descriptor objects
+descriptors = [];
+
+# load the raw data
+filedata = None;
+with open(RAW_TEXT_FILE, 'r') as file :
+  filedata = file.read();
+
+# starting regex
+regex = r'%s';
+
+# go through all surrounding words to add to regex
+for i in range(NUM_SURR_WORDS):
+    # add word before
+    regex = DESCRIPTOR_FORMAT_BEFORE + regex;
+    regex = regex + DESCRIPTOR_FORMAT_AFTER;
+
+for descriptor_string in descriptor_strings:
+    # search for all matches to descriptor, and also grab surrounding words
+    all_found = re.findall( regex % \
+            re.escape(descriptor_string), filedata);
+    if len(all_found) == 0:
+        print("descriptor: " + descriptor_string);
